c85340a716382ab20789c07ff77a90f74b6024d4
Raghav Vaid <raghavvaid@billericak12.com>
2021-11-18 19:45:23 -0500

    siejfoie

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -2,19 +2,52 @@ package com.company;
 
 public class Factorial {
 
-    public static int calcFactorial(int fac){
+    public static long calcFactorial(int fac){
         int ans = 1;
-        for (int x = fac; x > 0; x--){
-            ans = x*ans;
+        long x = 1;
+        while (fac >= ans){
+            x *= ans;
+            ans++;
         }
-        return ans;
+        return x;
     }
 
+    public static double calcE(){
+        double e = 1.0;
+        double olde = 0.0;
+        int frac = 1;
+        while (e - olde > .001){
+            olde = e;
+            e+=1.0/calcFactorial(frac);
+            frac++;
+        }
+        return e;
+    }
 
+    public static double calcEX(int n){
+        double e = 1.0;
+        double olde = 0.0;
+        int frac = 1;
+        int pwr = 1;
+        while (e-olde > .001){
+            olde = e;
+            e+= Math.pow(n,pwr)/calcFactorial(frac);
+            frac++;
+            pwr++;
+        }
+        return e;
+    }
 
     public static void main(String[] args) {
-        for (int x = 1; x<=10; x++){
+        for (int x = 1; x<=20; x++){
             System.out.println(calcFactorial(x));
+        }
+        System.out.println(" ");
+        System.out.println(calcE());
+        System.out.println(" ");
+        for (int x = 1; x <=5; x++){
+            double e = calcEX(x);
+            System.out.printf("e^%d is %2.3f \n", x, e);
         }
     }
 }
